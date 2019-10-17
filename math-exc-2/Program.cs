ffcb61f92b4451a9e70af85e7e819070a0367a93
ArzuHa <53233132+ArzuHa@users.noreply.github.com>
2019-10-17 11:46:31 +0400

    Add files via upload

---
+++ math-exc-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math_exc_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, temp, rev = 0;
+            int sum = 1;
+            Console.WriteLine("Reqem daxil edin:");
+            n = int.Parse(Console.ReadLine());
+            
+            // reverse any number
+
+            while (n != 0)
+            {
+                rev = (rev * 10) + (n % 10);
+                n = n / 10;
+            }
+            n = rev;
+
+            // split any number
+            Console.WriteLine("\n");
+
+
+            while (n!=0)
+            {
+                temp = n % 10;
+                    n = n / 10;
+                sum = sum * temp;
+
+                Console.WriteLine(temp);
+            }
+           
+            Console.WriteLine(sum);          
+            Console.ReadKey();
+           
+           
+        }
+        
+    }
+}
